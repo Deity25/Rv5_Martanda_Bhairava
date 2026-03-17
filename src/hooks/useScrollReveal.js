1f5ab75236f81a1d289067462d4cdcb6988780de
Gaurishankar Vhadle <vhadlegaurishankar@gmail.com>
2026-03-17 22:37:00 +0530

    Update useScrollReveal.js

--- src/hooks/useScrollReveal.js
+++ src/hooks/useScrollReveal.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+
 const SELECTOR =
   ".section-head h2, .section-head p, .panel, .table-wrap, .story-card, .song-card, .art-card, .timeline-item, .glossary-card, .stat-card";
 
